3479dc9e198d78689b612a67e6789afaff35dab7
Matthew York <myorkprogramming@gmail.com>
2023-07-09 10:22:18 -0700

    Rotate Arr

--- index.js
+++ index.js
@@ -30,3 +30,43 @@ console.log(reverse([1, 2, 3, 4, 5, 6]));
 
 //#######################################################################
 
+//2.
+// Rotate
+
+/* Implement rotateArr(arr, shiftBy) that accepts array and offset.
+Shift arr’s values to the right by that amount.
+‘Wrap-around’ any values that shift off array’s end to the other side,
+so that no data is lost. Operate in-place: given ([1,2,3],1),   
+change the array to [3,1,2]. Don’t use built-in functions.
+Second: allow negative shiftBy (shift L, not R).
+Third: minimize memory usage. With no new array, handle arrays/shiftBys in the millions.
+Fourth: minimize the touches of each element. */
+
+function rotateArr(arr, shiftBy) {
+    var temp = 0;
+    var count = 0;
+    if (shiftBy > 0) {
+        while (count < shiftBy) {
+            temp = arr[arr.length - 1];
+            for (var i = arr.length - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = temp;
+            count++;
+        }
+    }
+    else if (shiftBy < 0) {
+        while (count > shiftBy) {
+            temp = arr[0];
+            for (var i = 0; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length - 1] = temp;
+            count--;
+        }
+    }
+
+    return arr;
+}
+
+console.log(rotateArr([1, 2, 3, 4, 5, 6], 2));
